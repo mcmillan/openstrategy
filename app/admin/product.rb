b8cfd9d802b14dd95ae634102fb6668ecbce6e36
Josh McMillan <josh@joshmcmillan.co.uk>
2014-10-25 19:57:53 +0100

    Admin tweaks

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -6,19 +6,6 @@ ActiveAdmin.register Product do
     column :url
     actions
   end
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
+  
+  permit_params :category_id, :title, :description, :url
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = 'ffd76a2c8254463deaefc387355284373ce8d60d13451e3b80658ce17afe10088aacaf17216dae3ff63df44856eee0959a56616a89ad3b49bc161a4f771a7698'
+  config.secret_key = 'ffd76a2c8254463deaefc387355284373ce8d60d13451e3b80658ce17afe10088aacaf17216dae3ff63df44856eee0959a56616a89ad3b49bc161a4f771a7698'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
