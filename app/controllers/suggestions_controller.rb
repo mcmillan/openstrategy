d7a3c53cef84286638d974bc88f2ebae76623f89
Josh McMillan <josh@joshmcmillan.co.uk>
2015-01-02 14:28:57 +0000

    Add JSON endpoint for suggestion creation

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,19 +1,27 @@
 class SuggestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create], if: -> { request.format.json? }
+  before_action :add_wide_open_cors_headers, only: [:create], if: -> { request.format.json? }
+
   def new
     @suggestion = Suggestion.new
   end
-  
+
   def create
     @suggestion = Suggestion.new(suggestion_params)
-    
-    if @suggestion.save
-      redirect_to root_path
-    else
-      render 'new'
+
+    respond_to do |format|
+      if @suggestion.save
+        format.html { redirect_to root_path }
+        format.json { head :no_content }
+      else
+        format.html { render 'new' }
+        format.json { render json: { errors: @suggestion.errors.full_messages } }
+      end
     end
   end
-  
+
   private
+
   def suggestion_params
     params.require(:suggestion).permit(
       :category_id,
