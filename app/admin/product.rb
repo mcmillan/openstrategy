79df3042512321db9ed4bda7330cf465f73323f3
Josh McMillan <josh@joshmcmillan.co.uk>
2015-01-02 13:14:28 +0000

    Add click count into admin system

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -4,8 +4,17 @@ ActiveAdmin.register Product do
     column :title
     column :description
     column :url
+    column 'Clicks' do |p|
+      p.product_clicks.count
+    end
     actions
   end
-  
+
   permit_params :category_id, :title, :description, :url
+
+  controller do
+    def scoped_collection
+      end_of_association_chain.includes(:product_clicks)
+    end
+  end
 end
