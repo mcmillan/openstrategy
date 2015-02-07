70db9d6749a44262bd960776004a2ee534c5e8a6
Josh McMillan <josh@joshmcmillan.co.uk>
2015-02-07 15:12:22 +0000

    oh shit x 5

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    sign_in_and_redirect @user
+    sign_in @user
+    redirect_to 'http://openstrate.gy'
   end
 end
