7ad1bfaa34eb8c3a60b188ffeb6b250aba370c4a
Jesse the Game <jesse@jessethegame.net>
2015-02-23 13:28:55 +0000

    Introduce library category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   after_action :add_wide_open_cors_headers, if: -> { request.format.json? }
 
   def index
-    @categories = Category.all
+    @categories = Category.non_library
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   has_many :products
   validates :title, presence: true
   default_scope -> { order(:weight) }
+
+  scope :non_library, -> { where.not(id: [ENV['LIBRARY_CATEGORY_ID']].compact) }
 end
