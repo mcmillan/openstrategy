bb11d8ddb167ba0f6dd6d4fc93eb03c89893af7d
Josh McMillan <josh@joshmcmillan.co.uk>
2016-01-10 17:41:34 +0000

    Re-enable newsletter subscription

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,13 +3,19 @@ class EmailsController < ApplicationController
     email = params[:email]
     gb    = Gibbon::API.new
 
-    gb.lists.subscribe(
-      id: 'a15ef776d9',
-      email: { email: email },
-      double_optin: false,
-      update_existing: true
-    )
+    begin
+      gb.lists.subscribe(
+        id: 'a15ef776d9',
+        email: { email: email },
+        double_optin: false,
+        update_existing: true
+      )
+    rescue => e
+      return redirect_to root_url, alert: e.message
+    end
 
-    head :no_content
+    cookies.permanent[:subscribed_to_newsletter] = true
+
+    redirect_to root_url, notice: "Thanks! You're all signed up!"
   end
 end
